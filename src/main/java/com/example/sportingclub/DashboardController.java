82bc68842ea72ca8be82105c913a7daaf2308fb4
The-CapedCrusader <6209tanvir@gmail.com>
2025-03-25 10:07:49 +0600

    Initial commit to check

---
+++ src/main/java/com/example/sportingclub/DashboardController.java
@@ -0,0 +1,12 @@
+package com.example.sportingclub;
+
+import javafx.scene.control.ComboBox;
+
+public class DashboardController
+{
+    @javafx.fxml.FXML
+    private ComboBox firstCB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
