3ea908ffcee6fabadd471115b0d265ecfd18c59b
The-CapedCrusader <6209tanvir@gmail.com>
2025-04-15 04:12:52 +0600

    Dashboard Transfer Window Manager

--- src/main/java/cse213/cse213_sporting_club_operations/TanvirMahmud/DashboardClubManager.java
+++
@@ -1,7 +0,0 @@
-package cse213.cse213_sporting_club_operations.TanvirMahmud;
-
-public class DashboardClubManager
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
---
+++ src/main/java/cse213/cse213_sporting_club_operations/TanvirMahmud/DashboardTransferWindowManager.java
@@ -0,0 +1,42 @@
+package cse213.cse213_sporting_club_operations.TanvirMahmud;
+
+import javafx.event.ActionEvent;
+
+public class DashboardTransferWindowManager
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void goal2(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal1(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal8(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal7(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal6(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal5(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal4(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal3(ActionEvent actionEvent) {
+    }
+}
